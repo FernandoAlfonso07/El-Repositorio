5ca14528198d6bd71f00439a432c7219ea21e91d
FernandoAlfonso07 <148348192+FernandoAlfonso07@users.noreply.github.com>
2023-10-20 07:56:48 -0500

    v1.3

--- 20231020a/despliegue.php
+++ 20231020a/despliegue.php
@@ -1,4 +1,4 @@
 <?php 
 include("funciones.php"); //incluye el archvo de funcuines.
 
-echo consultar(); //llamar funcion que muestra todos los registros de una tabla.
+echo consultar('10'); //llamar funcion que muestra todos los registros de una tabla.
--- 20231020a/funciones.php
+++ 20231020a/funciones.php
@@ -1,19 +1,21 @@
 <?php
 /*
 *   Esta funcion se encarga de consultar los datos de la tabla de usuarios.
-* 
+*   @return Retorna la salida.
+* @param es un parametro opcional.
 */
-function consultar() //crea la funcion.
+function consultar($id=null) //crea la funcion.
 {
     $salida = ''; //Inicializa variable TEXTO o NUMERO
     $conexion = mysqli_connect("localhost", "root", "root", "bd_ejercicio_estudiantes1"); //Conectar con base de datos
     $sql = "select * from usuarios"; //CODIGO SQL en donde insertare datos a la tabla.   
+    if ($id !=null) $sql .= " where id_U = '$id'"; //Conecto por medio de una descicion una segunda funcion de un solo registro.
     $resultado = $conexion->query($sql); //Ejecutar lo que se pida en el sql.
 
     while ($fila = mysqli_fetch_array($resultado)) {
-        $salida .= '<b>ID: </b>' . $fila[0] . '<br>';
-        $salida .= '<b>NOMBRE: </b>' . $fila[1] . '<br>';
-        $salida .= '<b>SITIO WEB: </b>' . $fila[2] . '<br>';
+        $salida .= '<b>ID: </b>' . $fila[0] . '<br>'; //Concateno y muestra la linea deseada.
+        $salida .= '<b>NOMBRE: </b>' . $fila[1] . '<br>';//Concateno y muestra la linea deseada.
+        $salida .= '<b>SITIO WEB: </b>' . $fila[2] . '<br>';//Concateno y muestra la linea deseada.
     }
 
     $conexion->close();
