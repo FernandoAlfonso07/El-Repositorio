6023331815d7a4a205167eb063dc6d6214deda06
FernandoAlfonso07 <148348192+FernandoAlfonso07@users.noreply.github.com>
2023-10-20 08:20:46 -0500

    v1.4

--- 20231020a/despliegue.php
+++ 20231020a/despliegue.php
@@ -1,4 +1,4 @@
 <?php 
 include("funciones.php"); //incluye el archvo de funcuines.
 
-echo consultar('10'); //llamar funcion que muestra todos los registros de una tabla.
+echo consultar('10', '123456789'); //llamar funcion que muestra todos los registros de una tabla.
--- 20231020a/funciones.php
+++ 20231020a/funciones.php
@@ -1,21 +1,25 @@
 <?php
 /*
 *   Esta funcion se encarga de consultar los datos de la tabla de usuarios.
+*   @param_id es un parametro opcional.
+*   @param_c es un parametro opcional de contraseña.
 *   @return Retorna la salida.
-* @param es un parametro opcional.
 */
-function consultar($id=null) //crea la funcion.
+function consultar($id = null, $c = null) //crea la funcion.
 {
     $salida = ''; //Inicializa variable TEXTO o NUMERO
     $conexion = mysqli_connect("localhost", "root", "root", "bd_ejercicio_estudiantes1"); //Conectar con base de datos
     $sql = "select * from usuarios"; //CODIGO SQL en donde insertare datos a la tabla.   
-    if ($id !=null) $sql .= " where id_U = '$id'"; //Conecto por medio de una descicion una segunda funcion de un solo registro.
+    if ($id != null) $sql .= " where id_U = '$id' "; //Conecto por medio de una descicion una segunda funcion de un solo registro.
+    if ($c != null) $sql .= " and contraseña = '$c'"; //incluye si se usa la contraseña o no.
+    
+     //Conecto por medio de una descicion una segunda funcion de un solo registro.
     $resultado = $conexion->query($sql); //Ejecutar lo que se pida en el sql.
 
     while ($fila = mysqli_fetch_array($resultado)) {
         $salida .= '<b>ID: </b>' . $fila[0] . '<br>'; //Concateno y muestra la linea deseada.
-        $salida .= '<b>NOMBRE: </b>' . $fila[1] . '<br>';//Concateno y muestra la linea deseada.
-        $salida .= '<b>SITIO WEB: </b>' . $fila[2] . '<br>';//Concateno y muestra la linea deseada.
+        $salida .= '<b>NOMBRE: </b>' . $fila[1] . '<br>'; //Concateno y muestra la linea deseada.
+        $salida .= '<b>SITIO WEB: </b>' . $fila[2] . '<br>'; //Concateno y muestra la linea deseada.
     }
 
     $conexion->close();
